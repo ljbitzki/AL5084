0f7f21949ed3afb2fabc42b382028ad08de12a1b
Leonardo Bitzki <ljbitzki@gmail.com>
2025-10-01 15:10:09 -0300

    Ajuste de parâmetro

--- al5084.py
+++ al5084.py
@@ -14,7 +14,7 @@ def main():
     ap_cap.add_argument("-i", "--iface", required=True, help="Interface (ex: enp0s3, eth0, etc)")
     ap_cap.add_argument("-d", "--duration", type=int, default=60, help="Duração (s)")
     ap_cap.add_argument("-o", "--out", required=True, type=Path, help="Arquivo .pcap de saída")
-    ap_cap.add_argument("--snaplen", type=int, default=96, help="Snaplen (bytes)")
+    ap_cap.add_argument("-s", "--snaplen", type=int, default=96, help="Snaplen (bytes)")
     args = ap.parse_args()
 
     if args.cmd == "capture":
